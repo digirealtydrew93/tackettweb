cb4994879018cb3732ba9e08769112c5fa603851
Andrew Green <doggieboy202@gmail.com>
2025-11-15 23:31:54 -0500

    Initial scaffold for Tackett Brothers Hauling

---
+++ site/functions/submit.js
@@ -0,0 +1,12 @@
+﻿const twilio = require("twilio");
+exports.handler = async (event) => {
+  try {
+    const data = JSON.parse(event.body);
+    const messageBody = `New Pickup Request: ${JSON.stringify(data)}`;
+    const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
+    await client.messages.create({ from: process.env.TWILIO_NUMBER, to: process.env.BIDDER_NUMBER, body: messageBody });
+    return { statusCode: 200, body: JSON.stringify({ message: "Request sent" }) };
+  } catch (err) {
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+  }
+};
